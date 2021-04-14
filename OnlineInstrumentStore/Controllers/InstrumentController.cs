85837d59c26181bc5ed758639aa9991361233e8e
HoratiuSpawn <horatiu.botoroaga@gmail.com>
2021-04-14 21:46:44 +0300

    Added Instrument Controler

--- OnlineInstrumentStore/Controllers/InstrumentController.cs
+++ OnlineInstrumentStore/Controllers/InstrumentController.cs
@@ -11,8 +11,7 @@ namespace OnlineInstrumentStore.Controllers
     public class InstrumentController : Controller
     {
         private InstrumentRepository instrumentRepository = new InstrumentRepository();
-
-        [AllowAnonymous]
+                
 
         // GET: Instrument
         public ActionResult Index()
@@ -32,7 +31,7 @@ namespace OnlineInstrumentStore.Controllers
         // GET: Instrument/Create
         public ActionResult Create()
         {
-            return View();
+            return View("CreateInstrument");
         }
 
         // POST: Instrument/Create
@@ -41,13 +40,16 @@ namespace OnlineInstrumentStore.Controllers
         {
             try
             {
-                // TODO: Add insert logic here
+                InstrumentModels instrumentModels = new InstrumentModels();
+                UpdateModel(instrumentModels);
+
+                instrumentRepository.InsertInstrument(instrumentModels);
 
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View("CreateInstrument");
             }
         }
 
